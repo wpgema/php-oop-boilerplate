1c530d6ada3fb31cf8a0804fcb2d9cceb61e2e73
m gema maulana <mgemamaulana04@gmail.com>
2024-10-03 22:19:50 +0700

    Melakukan penambahan user guide pada halaman dashboard untuk memudahkan pengguna dalam menggunakan source code

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,1 +1,5 @@
-    <h1>Welcome to your app</h1>
+<div class="container">
+    <h1>welcome to your web app</h1>
+    <h3>Go to folder application/config and find file config.php</h3>
+    <p>in this file you can change configuration for your app</p>
+</div>
